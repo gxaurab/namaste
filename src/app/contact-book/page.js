9c37678115bf30ec2c8519c6ee75d8fe4107b7b4
gxaurab <oe.gaurab@gmail.com>
2024-08-06 17:00:35 +0545

    idk

--- src/app/contact-book/page.js
+++ src/app/contact-book/page.js
@@ -1,9 +1,15 @@
 
 
-const contact_book = () => {
+export default function ContactBook() {
   return (
-    <div>contact us book us page</div>
+    <div className="mt-28 bg-red-100 ">
+      <h2>Hello, this is contact book page<br></br></h2>
+      <h2>Hello, this is contact book page<br></br></h2>
+
+      <h2>Hello, this is contact book page<br></br></h2>
+
+      <h2>Hello, this is contact book page<br></br></h2>
+
+    </div>
   )
 }
-
-export default contact_book
